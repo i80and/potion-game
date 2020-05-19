407837032abe4127db054e9ae49c62a352258fc7
Heli Skunk <i80and@foxquill.com>
2020-05-19 19:56:10 -0400

    Populate trees (#2)
    * Populate trees
    
    * Add missing island assets

--- scripts/GameWorld.gd
+++ scripts/GameWorld.gd
@@ -4,7 +4,3 @@ extends Spatial
 # Declare member variables here. Examples:
 onready var _character = get_node(@"PlayerCharacter")
 onready var _piehud = get_node(@"CanvasLayer/PieHUD")
-
-
-func _ready() -> void:
-	pass
